495130f6e0fb65dc3b73725ee331357d253d49f0
Clint <cbmarch88@gmail.com>
2022-12-01 21:40:42 -0600

    Add project files.

---
+++ AOC2022/Elf.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2022
+{
+    public class Elf
+    {
+        public List<int> Calories { get; set; } = new List<int>();
+        public int TotalCalories { get; set; }
+    }
+}
---
+++ AOC2022/Program.cs
@@ -0,0 +1,24 @@
+﻿using AOC2022;
+
+
+List<Elf> elves = new List<Elf>();
+Elf elf = new Elf();
+foreach (string calorie in File.ReadAllLines("day1.txt"))
+{
+    if (string.IsNullOrEmpty(calorie))
+    {
+        elf.TotalCalories = elf.Calories.Sum();
+        elves.Add(elf);
+        elf = new Elf();
+    }
+    else
+    {
+
+        elf.Calories.Add(int.Parse(calorie));
+    }
+}
+
+var highestCalories = elves.Max(t => t.TotalCalories);
+var sortElvesDescending = elves.OrderByDescending(c => c.TotalCalories).ToList();
+int topCalories3 = sortElvesDescending[0].TotalCalories + sortElvesDescending[1].TotalCalories + sortElvesDescending[2].TotalCalories;
+Console.WriteLine(topCalories3);
